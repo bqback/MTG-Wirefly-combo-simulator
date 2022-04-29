e64783cc35df8a8d8cf916ca1d9bd7f6a314c063
Mike <lolwut-lol@yandex.ru>
2022-04-30 00:14:01 +0300

    Added show mode, doc strings in plot_data, fixed __name__ namespace check

--- mtg_test.py
+++ mtg_test.py
@@ -1,6 +1,6 @@
 import random
 import matplotlib.pyplot as plt
-import numpy as np
+import os.path
 from dataclasses import dataclass
 from typing import Dict
 
@@ -14,6 +14,12 @@ class Result:
 
 
 def combo(attempts: int, hp: int) -> Result:
+    """
+
+    :param attempts:
+    :param hp:
+    :return:
+    """
     wireflies = 0
     seq = ""
 
@@ -32,21 +38,78 @@ def combo(attempts: int, hp: int) -> Result:
     return Result(False, -1, -1, "")
 
 
-def plot_data(sim_data: Dict, xlabel: str, filename: str) -> None:
-    fig = plt.figure()
+def next_name(filename: str) -> str:
+    name, ext = os.path.splitext(filename)
+    counter = 0
+    new_name = filename
+    while os.path.exists(filename):
+        counter += 1
+        new_name = f"{name} (counter){ext}"
+    return new_name
+
+
+def plot_data(
+        sim_data: Dict,
+        xlabel: str,
+        display_mode='save',
+        filename='graph.png',
+        overwrite=True
+) -> None:
+    """
+
+    Plots provided data, either saving it to a file or showing in a new window
+
+    :param sim_data:
+        Data, formatted as {x_1: y_1, x_2: y_2, ...}
+    :param xlabel:
+        String that will be used as the x-axis label
+    :param display_mode:
+        Either "save" or "show";
+        "save": the graph is saved to a file;
+        "show": the graph is displayed in a window;
+        default: save
+    :param filename:
+        (If display_mode == save)
+        Filename for the graph image;
+        default: graph.png
+    :param overwrite:
+        (If display_mode == save)
+        If True, the graph file will be silently overwritten;
+        if False, the graph file will get an additional sequential number;
+        default: True
+
+    :return:
+        None
+
+    :raises:
+        ValueError if the wrong display_name value is provided
+    """
+
+    dpi = plt.rcParams["figure.dpi"]
+    fig = plt.figure(figsize=(1280 / dpi, 960 / dpi))
     ax = fig.add_axes([0, 0, 1, 1])
     rects = ax.bar(sim_data.keys(), sim_data.values())
     ax.set_xlabel(xlabel)
+    ax.set_ylabel("Number of occurrences")
     ax.set_xticks(list(sim_data.keys()))
     ax.bar_label(rects, padding=3)
 
-    plt.savefig(filename, bbox_inches='tight')
+    fig.tight_layout()
+
+    if display_mode == "save":
+        if os.path.exists(filename) & (not overwrite):
+            filename = next_name(filename)
+        plt.savefig(filename, bbox_inches='tight')
+    elif display_mode == "show":
+        plt.show()
+    else:
+        error_msg = f"display_mode must be either show or save (provided value: {display_mode})"
+        raise ValueError(error_msg)
 
 
-if __name__ == "__main__":
-
-    n = 10000  # number of simulations
-    flips = 100  # number of coin flips per simulation
+def sim():
+    n = 10000000  # number of simulations
+    flips = 1000  # number of coin flips per simulation
     opp_hp = 4  # opponent's starting HP
     success = 0  # successful combo counter
     max_wf = 0  # maximum amount of wireflies in a successful combo
@@ -81,5 +144,11 @@ if __name__ == "__main__":
         f"Max flips: {max_flips}, with sequence\n{max_sequence}"
     )
 
-    plot_data(wf_data, "Wireflies used", "wf_graph.png")
-    plot_data(flip_data, "Flips used", "flip_graph.png")
+    plot_data(wf_data, "Wireflies used", "save", "wf_graph.png")
+    # plot_data(wf_data, "Wireflies used", "show")
+    plot_data(flip_data, "Flips used", "save", "flip_graph.png")
+    # plot_data(flip_data, "Flips used", "show")
+
+
+if __name__ == "__main__":
+    sim()
